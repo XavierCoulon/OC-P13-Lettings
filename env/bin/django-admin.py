1d2732ca4e60ca234cd8625674a7fde140d5c791
XavierCoulon <xcoulon.mail@gmail.com>
2022-05-11 14:27:56 +0200

    circleCI test

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/xco/Documents/XCO/OC/P13_OC_Lettings/Python-OC-Lettings-FR/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
