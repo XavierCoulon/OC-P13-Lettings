d26e78927984546b359ce3526bd4744a0db34dbb
XavierCoulon <xcoulon.mail@gmail.com>
2022-05-17 14:53:18 +0200

    untrack env

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/xco/Documents/XCO/OC/P13_OC_Lettings/Python-OC-Lettings-FR/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
