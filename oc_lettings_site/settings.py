cdade4f2f981f7b0ec05fad919c6fafbb850914b
XavierCoulon <xcoulon.mail@gmail.com>
2022-05-18 11:22:37 +0200

    SENTRY_DNS

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = '/static/'
 django_on_heroku.settings(locals(), test_runner=False)
 
 sentry_sdk.init(
-    dsn=os.getenv("SENTRY_KEY"),
+    dsn=os.getenv("SENTRY_DNS"),
     integrations=[DjangoIntegration()],
 
     # Set traces_sample_rate to 1.0 to capture 100%
